6bb6f6107bf92be7c1f57824ea927a2c400ed280
Dudododo <a752601260@gmail.com>
2020-03-27 15:03:02 +0800

    添加项目文件。

---
+++ buffing.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture cap(0);
+
+	double scale = 0.5;
+
+	double i_minH = 0;
+	double i_maxH = 20;
+	double i_minS = 43;
+	double i_maxS = 255;
+	double i_minV = 55;
+	double i_maxV = 255;
+
+	while (1)
+	{
+		Mat frame;
+		Mat hsvMat;
+		Mat medMat;
+		Mat dipMat;
+		Mat detectMat;
+
+		//缩放
+		cap >> frame;
+		Size ResImgSiz = Size(frame.cols * scale, frame.rows * scale);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+
+		//转换图像格式
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+
+		rFrame.copyTo(detectMat);
+
+		//限定范围
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+
+		//均值滤波
+		cv::GaussianBlur(rFrame, medMat, cv::Size(5, 5), 3, 3);
+
+		rFrame.copyTo(dipMat);
+		medMat.copyTo(dipMat, detectMat);
+
+		imshow("white: in the range", detectMat);
+		imshow("display", dipMat);
+		imshow("frame", rFrame);
+
+		waitKey(30);
+	}
+}
+
